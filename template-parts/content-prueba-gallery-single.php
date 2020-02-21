3cad9fb7a3290ba4c03690742f73bb043a40a1fb
cmunozm <cmunoz@banchilevida.cl>
2020-02-20 23:50:56 -0300

    optimizando single php

--- template-parts/content-prueba-gallery-single.php
+++ template-parts/content-prueba-gallery-single.php
@@ -1,38 +1,48 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article <?php post_class( 'row' );?>> 
 <div class="col-12 mt-3">
-
+    <small>prueba-gallery-single.php</small>
     <h3>Ficha técnica:</h3>
     <hr class="my-1">    
     <p><?php the_field( 'description' ); ?></p>
 
 </div>
 <div class="col-12">
-    <?php $images = get_field('gallery');
-        if( $images ): ?>
-        <div id="slider" class="flexslider customflex">
-            <ul class="slides">
-            <?php foreach( $images as $image ): ?>
+    <?php //$images = get_field('gallery'); ?>
+    <?php 
+$images = get_field('gallery');
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+if( $images ): ?>
+    <ul>
+        <?php foreach( $images as $image ): ?>
+
+        <?php 
+            $thumb1 = get_field( 'thumbnail_1' );
+            $thumb2 = get_field( 'thumbnail_2' );
+            $thumb3 = get_field( 'thumbnail_3' );
+            $thumb4 = get_field( 'thumbnail_4' ); 
+            $todos = $thumb1 || $thumb2 || $thumb3 || $thumb4;
+        ?>
+
+
+            
+                <?php if( $image || $todos ): ?>
                 <li>
-                    <a class="link-thumb-single-gallery" href="<?php echo esc_url($image['url']); ?>">
-                    <!-- <img src="<?php // echo esc_url($image['sizes']['large']); ?>" alt="<?php // echo esc_attr($image['alt']); ?>" /> -->
-                        <div class="thumbnail-single-gallery" style="background: url(<?php echo esc_url($image['sizes']['large']); ?>) no-repeat center center;">                    
-                    </div>
-                    </a>
+                    <img src="<?php echo esc_url($image['sizes']['large']); ?>" alt="<?php  echo esc_attr($image['alt']); ?>" />            
                 </li>
-            <?php endforeach; ?>
-            </ul>
-        </div>
-        <div id="carousel" class="flexslider">
-            <ul class="slides">
-                <?php foreach( $images as $image ): ?>
-                    <li>
-                        <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="Thumbnail of <?php echo esc_url($image['alt']); ?>" />
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>    
-    <?php endif; ?>
+                <?php else: ?>
+                <?php //unset($thumb3, $thumb4); ?>
+                 <li>
+                    hola mundo!        
+                </li>
+            <?php endif; ?>
+
+
+
+        <?php endforeach; ?>
+    </ul>
+     <?php endif; ?>
+
 </div>
 </article>
 
@@ -43,20 +53,18 @@
 
 
 <?php
-/*$image = get_field( 'gallery' ); 
+ $image = get_field( 'gallery' ); 
 echo '<code>';
 echo '<pre>';
       var_dump( $image ); 
 echo '</pre>';
-echo '</code>';*/
+echo '</code>';
 ?>
-<?php endwhile; else: ?>
-    <article>
+<?php endwhile;  else: ?>
+ <article>
         <header>
             <h1>Lo lamentamos</h1>
         </header>
         <p>No hay contenidos que mostrar :( </p>
-    </article>
+    </article> 
 <?php endif; ?>
-
-
